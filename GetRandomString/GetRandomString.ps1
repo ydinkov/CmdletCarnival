6eedcaaed2fded2e363e6e9a1d75f3184b57bce0
Yasen <ydinkov@gmail.com>
2022-03-29 12:33:07 +0200

    first commit

---
+++ GetRandomString/GetRandomString.ps1
@@ -0,0 +1,108 @@
+function Get-Random-String {
+    <#    
+    .SYNOPSIS
+    Generates a random string with an array of customisations
+    #>
+
+    <#    
+    .DESCRIPTION
+    This cmdlet generates a random string based on user customisation.
+    #>
+
+  [CmdletBinding()]
+  [Alias('rndmstr')]
+  param(
+    <#    
+    .EXAMPLE
+    rndmstr 32 
+    ukmshszqvkwhivrevecpsrnqnvpjuvpkt
+    #>
+
+    <#    
+    .EXAMPLE
+    rndmstr 32 -UseUpperCase
+    LSlBajOMOAsyCeHipDWOiblIbiOrGSkfu
+    #>
+    <#    
+    .EXAMPLE
+    rndmstr 32 -UseUpperCase -UseNumbers
+    FYyU7bIUoFKJr3975ZAJc8jeCnVhjlZqT
+    #>
+
+    <#    
+    .EXAMPLE
+    rndmstr 32 -UseUpperCase -UseLoweCase:$false
+    ARKDMQJFILYWYTAZNSRQOUFYLFTMIZEWV
+    #>
+
+    <#    
+    .EXAMPLE
+    rndmstr 32 -UseUpperCase -UseNumbers -UseSpecialCharacters
+    bFNC1D1K*ng37neaYYpT0J985+ZCaC47h
+    #>
+         
+    
+
+    [Parameter(        
+        Mandatory=$true,
+        HelpMessage="The character length of the requested random string"
+    )]
+    [int] $StringLength,
+
+    [Parameter(HelpMessage="Adds Lower-Case latin characters to the string pool")]
+    [switch] $UseLowerCase = $true,
+
+    [Parameter(HelpMessage="Adds Upper-Case latin characters to the string pool")]
+    [switch] $UseUpperCase = $false,
+
+    [Parameter(HelpMessage="Adds numeric characters to the string pool")]
+    [switch] $UseNumbers = $false,
+
+    [Parameter(HelpMessage="Adds the special characters: '!@#$%^&*_+-' to the string pool")]
+    [switch] $UseSpecialCharacters = $false,
+
+    [Parameter(HelpMessage="Characters here will be added to the string pool")]
+    [string] $IncludeCharacters = "",
+
+    [Parameter(HelpMessage="Characters here will be removed to the string pool")]
+    [string] $ExcludeCharacters = ""
+  )
+    if(
+     !($UseLowerCase) -and
+     !($UseUpperCase) -and
+     !($UseNumbers) -and
+     !($UseSpecialCharacters) -and
+     ($IncludeCharacters -eq "")
+     ){
+        throw "No Characters allowed. Please use some of the parameter flags to enable character sets."
+     }
+    # Initate character sets
+    $Letters = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
+    $Numbers = "0123456789"
+    $SpecialCharacters = "!@#$%^&*_+-"
+    
+    # Populate the pool so that each character has a chance to be picked
+    $Pool = ""
+    for ($i = 0 ; $i -lt $StringLength ; $i++){
+        if($UseUpperCase) {$Pool+=$Letters}
+        if($UseLowerCase) {$Pool+=$Letters.ToLower()}
+        if($UseNumbers){$Pool+=$Numbers }
+        if($UseSpecialCharacters){$Pool+=$SpecialCharacters}
+        $Pool+=$IncludeCharacters
+    }
+
+    # Remove Excluded characters
+    foreach ($Char in $ExcludeCharacters.toCharArray()) { $Pool= $Pool.Replace($Char,"")}
+    if($Pool -eq ""){
+        throw "No Characters allowed. All available characters were excluded by user."
+    }
+
+    # Sort by Get-Random to shuffle (Get-Random is a secure random)
+    $Shuffle = $($Pool.tochararray() | Sort-Object {Get-Random})
+    
+    # Return the first N charachters as a string
+    return $Shuffle[0..$StringLength] -join ''
+}
+
+
+
---
+++ InvokeSomething/InvokeSomething.ps1
@@ -0,0 +1,61 @@
+﻿function Invoke-Something {
+    [CmdletBinding()]
+    [Alias('somethingsomething')]
+    param(
+    [Parameter(
+        Mandatory=$true,
+        HelpMessage="This message describes the action that the user should take or that will be automated"
+        )]
+    [string] $Message,
+
+    [Parameter(HelpMessage="Expression string that will be invoked with 'Invoke-Expression' to automate the task ")]
+    [string] $Expression,
+
+    [Parameter(HelpMessage="Expression string that will be invoked to evaluate wether the required state has been reached. Expression must resolve to `$true or `$false. If no evaluation is provided, the script will always succeed")]
+    [string] $Evaluate,
+    
+    [Parameter(HelpMessage="Number of evaluations that will be performed until step fails")]
+    [int] $MaxEvaluations = 60,
+    
+    [Parameter(HelpMessage="Time between evaluations")]
+    [double] $Inteval = 1.0,
+
+    [Parameter(HelpMessage="Continue to the next step if this step fails")]
+    [switch] $ContinueOnFailure, #TODO
+
+    [Parameter(HelpMessage="If the evaluation fails, then re-execute the action instead of only retrying the evaluation")]
+    [switch] $RetryAction #TODO
+    
+    )
+
+
+    $Type = if($null -eq $Expression){"*AUTOMATED*"}else {"*MANUAL*"}
+    Write-Host $Type -ForeGroundColor Yellow
+    Write-Host Message: $Message -BackGroundColor White -ForeGroundColor Black
+    if(($null -ne $Expression) -and ($Expression -ne "")){        
+        Write-Host Execute? -NoNewLine
+        Write-Host `t`"$Expression`" -ForeGroundColor Cyan
+        Read-Host
+        Invoke-Expression -Command $Expression
+    }
+
+    if(($WaitUntil -ne "") -and ($WaitUntil -ne $null)){    
+        $Success = $false
+        $Retries = 0
+        while(($Success -ne $true) -and ($Retries -le $MaxEvaluations)){
+            $Success = Invoke-Expression -Command $WaitUntil
+            if($Success ){
+                Write-Host $WaitUntil -ForeGroundColor Cyan
+                Write-Host PASSED! ✅ -ForeGroundColor Green
+            }
+            else{v
+                Write-Host $WaitUntil -ForeGroundColor Cyan
+                Write-Host "Pending...(" $Retries"/" $MaxEvaluations ") ⌛" -ForeGroundColor DarkGray
+                $Retries = $Retries + 1
+                Start-Sleep -Seconds $Inteval
+
+            }
+        }
+    }
+    Read-Host "Continue?"
+}
