1912a804f6ca7b579bd0cdefe5dffea37440f4a9
Yasen <ydinkov@gmail.com>
2022-04-11 16:50:21 +0200

    Added ServiceBus Commander

---
+++ ServiceBusCommander/README.md
@@ -0,0 +1,34 @@
+# ServiceBusCommander ![](logo.png)
+This cmdlet lets the user interact with Azure Service bus namespaces.
+
+The code for generating a sas token is from [Emanuel Palm's excellent blog](https://pipe.how/send-servicebusmessage/)
+
+## Installation
+
+The modules requires the following .NET classes:
+- System.Web.HttpUtility
+- DateTimeOffset
+- Text.Encoding
+- Convert
+
+Download it from [Powershell Gallery](https://www.powershellgallery.com/packages/ServiceBusCommander/)
+or  run:
+```ps1
+Install-Module -Name ServiceBusCommander
+```
+
+## Send-QueueMessage
+
+This commands seds a raw message to a target service bus queue
+
+```ps1
+#Send single message
+Send-QueueMessage -ConnectionString <servicebus queue connectionString> -ContentType json -Body "{'MessageId':111,'Message':'bar'}"
+
+#Send from csv file
+Import-Csv Customers.csv | ForEach-Object { 
+    $Body = ConvertTo-Json $_
+    Send-QueueMessage -ConnectionString $ConnectionString -Body $Body
+}
+
+```
---
+++ ServiceBusCommander/ServiceBusCommander.psm1
@@ -0,0 +1,53 @@
+﻿<#    
+.SYNOPSIS
+Retreives a service bus sastoken based on a provided connection string
+#>
+function Get-SasToken{
+    param(
+        [string]$ConnectionString,        
+        [string]$EnityName = "",
+        [int]$TokenValidFor = 3600    
+    )
+        $Pattern = 'Endpoint=(.+);SharedAccessKeyName=(.+);SharedAccessKey=(.+);EntityPath=(.+)'
+        ([uri]$Endpoint),$PolicyName,$Key,$Enity = ($ConnectionString -replace $Pattern,'$1;$2;$3;$4') -split ';'    
+        if($EnityName -ne "") {$Enity = $EnityName}
+        $UrlEncodedEndpoint = [System.Web.HttpUtility]::UrlEncode($Endpoint.OriginalString)
+        $Expiry = [DateTimeOffset]::Now.ToUnixTimeSeconds() + $TokenValidFor
+        $RawSignatureString = "$UrlEncodedEndpoint`n$Expiry"    
+        $HMAC = New-Object System.Security.Cryptography.HMACSHA256
+        $HMAC.Key = [Text.Encoding]::ASCII.GetBytes($Key)
+        $HashBytes = $HMAC.ComputeHash([Text.Encoding]::ASCII.GetBytes($RawSignatureString))
+        $SignatureString = [Convert]::ToBase64String($HashBytes)
+        $UrlEncodedSignatureString = [System.Web.HttpUtility]::UrlEncode($SignatureString)
+        
+        return "SharedAccessSignature sig=$UrlEncodedSignatureString&se=$Expiry&skn=$PolicyName&sr=$UrlEncodedEndpoint"
+}
+
+<#    
+.SYNOPSIS
+Sends a message to service bus queue
+#>
+function Send-QueueMessage
+{
+    param(
+        [string]$ConnectionString,
+        [string]$Body,
+        [string]$QueueName = "",
+        [int]$TokenValidFor = 3600    
+    )
+    $SASToken = Get-SasToken -ConnectionString $ConnectionString -EnityName $QueueName    
+    $Params = @{
+        Uri = "https://$($Endpoint.Host)/$($QueueName)/messages"
+        ContentType = 'application/json;charset=utf-8'
+        Method = 'Post'
+        Body = $Body
+        Headers = @{
+            'Authorization' = $SASToken
+        }
+    }
+    Start-Sleep -Seconds 1
+    Invoke-RestMethod @Params
+}
+
+
+
